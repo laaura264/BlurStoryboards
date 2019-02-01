0de8672afc7ecd2b6919f790f5ac4320b9f73d98
laaura264 <32044280+laaura264@users.noreply.github.com>
2019-02-01 12:16:58 +0100

    init commit

--- BlurStoryboards/ViewController.swift
+++ BlurStoryboards/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lblVibrancy: UILabel!
+    @IBOutlet weak var segmented: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Crear el efecto blur a la imagen
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = imgView.bounds //Para que sea de igual tamaño que la imagen
+        imgView.addSubview(blurView)
+        
+        //Crear el efecto vibrancy a la etiqueta
+        let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+        let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+        vibrancyView.frame = imgView.bounds
+        vibrancyView.contentView.addSubview(lblVibrancy)
+        vibrancyView.contentView.addSubview(segmented)
+        
+        blurView.contentView.addSubview(vibrancyView)
+        
     }
 
 
